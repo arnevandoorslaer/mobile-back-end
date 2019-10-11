10d11e8f525275b26ae87a8809fa07d5dcd212aa
arnevandoorslaer <arne.vandoorslaer@student.ucll.be>
2019-10-11 11:52:39 +0200

    service progress

--- src/main/java/be/model/CleverService.java
+++ src/main/java/be/model/CleverService.java
@@ -6,6 +6,8 @@ import be.db.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class CleverService {
     @Autowired
@@ -15,5 +17,43 @@ public class CleverService {
 
     public CleverService(){
 
+    }
+
+    public ArrayList<User> getUsers(){
+        return (ArrayList<User>) userRepository.findAll();
+    }
+
+    public void addUser(User user){
+        userRepository.save(user);
+    }
+
+    public void removeUser(User user){
+        userRepository.delete(user);
+    }
+
+    public void updateUser(User user){
+        userRepository.save(user);
+    }
+
+    public ArrayList<Event> getEvents(){
+        return (ArrayList<Event>) eventRepository.findAll();
+    }
+
+    public void addEvent(Event event){
+        eventRepository.save(event);
+    }
+
+    public void removeEvent(Event event){
+        eventRepository.delete(event);
+    }
+
+    public void updateEvent(Event event){
+        eventRepository.save(event);
+    }
+
+    public void addParticipant(long userid,long eventid){
+        Event temp = eventRepository.getOne(eventid);
+        temp.addParticipant(userRepository.getOne(userid));
+        updateEvent(temp);
     }
 }
