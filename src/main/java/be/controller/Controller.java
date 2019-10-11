9b52f408a581a2fa1c7e9b2164105c2bbbc57014
lem-onade <taelemansst@gmail.com>
2019-10-11 10:55:35 +0200

    progress

--- src/main/java/be/controller/Controller.java
+++ src/main/java/be/controller/Controller.java
@@ -3,23 +3,31 @@ package be.controller;
 import be.model.CleverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
 public class Controller {
+
     @Autowired
     CleverService service;
 
     public Controller() {
-        //get event
-
-        //add event
-
-        //add user
-
-        //get user
-
-        //add user to event
     }
+
+    @GetMapping("/")
+    public int root() {
+        return 1;
+    }
+
+    //get event
+
+    //add event
+
+    //add user
+
+    //get user
+
+    //add user to event
 }
